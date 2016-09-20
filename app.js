86e4fa44423e08844165ea50fb18d97364285b23
fvalka <fvalka@infosec.at>
2016-09-20 22:05:48 +0200

    Food Application

---
+++ app.js
@@ -0,0 +1,36 @@
+/**
+ * Created by fvalka on 20.09.2016.
+ */
+(function () {
+    'use strict';
+
+    angular.module('LunchCheck',[]).
+        controller('LunchCheckController', LunchCheckController);
+
+    // protects the code from minification
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope) {
+        $scope.checkIfTooMuch = function () {
+            var foodInApp = $scope.food;
+
+            if(foodInApp === undefined || foodInApp == '') {
+                $scope.message = 'Please provide some food!';
+                return;
+            }
+
+            var arrayOfFood = splitCsv(foodInApp);
+
+            if(arrayOfFood.length > 3) {
+                $scope.message = 'Too much!';
+            } else {
+                $scope.message = 'Enjoy!';
+            }
+        };
+
+        function splitCsv(string) {
+            return string.split(',');
+        }
+
+    };
+
+})();
